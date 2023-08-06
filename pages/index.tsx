d72d8e1f62ab1767cd82aa5ef81ed0b8c7cc0bbb
Emerald <90080194+emeraldcr@users.noreply.github.com>
2023-08-06 03:04:41 -0600

    Revert "Update to pricing"
    This reverts commit f8f638e0c77f9db183bbad5f2addb2cfb40e8e2e.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
-import Pricing from './header';
+
+import { tours, defaultTitle } from '../src/const';
+import { Tour } from '../src/types'
+import { displayName } from '../src/util';
+import Header from './header';
+
+
 
 export default function HomePage() {
 
   return (
     <div>
-      <Pricing />
+      <Header title={defaultTitle} />
+      <ul>
+        {tours.map((tour: Tour) => (
+          <li key={tour.name}>{displayName(tour)}</li>
+        ))}
+      </ul>
     </div>
   );
 }
