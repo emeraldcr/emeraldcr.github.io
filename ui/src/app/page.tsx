a10864edf1b8275814e87c60bf6ff9383f78afec
Katherine <ciudadesmeraldacr@gmail.com>
2023-10-02 12:57:31 -0600

    Clean and View

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,12 +1,12 @@
 
-import Reserva from './reservas/reserva'
-import Center from './divTypes/center'
-import Panel from './divTypes/panel'
+import Reserva from '../../pages/reservas/reserva'
+import Panel from '../../pages/type/panel'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+      <Reserva></Reserva>
+      <Panel></Panel>
     </main>
   )
 }
